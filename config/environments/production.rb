d352e52727c6c7eccae5ebe81edd6dd57088f6a3
Ben Leadholm <b.leadholm@sportradar.com>
2017-10-02 07:30:54 -0500

    update config files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
-
+  config.assets.raise_runtime_errors = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
@@ -47,7 +47,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :DEBUG
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -1,5 +1,11 @@
 # from https://www.chrisblunt.com/rails-3-how-to-autoload-and-autorequire-your-custom-library-code/
 
 require 'active_record/add_reset_pk_sequence_to_base'
+require 'mma/banks/utils/rate_matcher'
+require 'mma/banks/wells_fargo/rate_sheet/wells_fargo_logic'
+require 'mma/banks/wells_fargo/rate_sheet/wells_fargo_conforming_pricing'
+require 'mma/banks/wells_fargo/rate_sheet/wells_fargo_conforming_adjusters'
+require 'mma/banks/wells_fargo/regular_adjusters/adjusters'
+require 'mma/excel/excel_utils'
 require 'mma/excel/load_excel'
 require 'mma/excel/parse_excel'
