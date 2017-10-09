3fd970cface8f627a4d1c0bc653f945fdf50f5d8
Ben Leadholm <b.leadholm@sportradar.com>
2017-10-09 07:08:06 -0500

    change migration from the verb change to verbs up and down

--- db/migrate/20171007201123_rename_srp_in_wells_fargo_srp_sheet.rb
+++ db/migrate/20171007201123_rename_srp_in_wells_fargo_srp_sheet.rb
@@ -1,5 +1,13 @@
 class RenameSrpInWellsFargoSrpSheet < ActiveRecord::Migration
-  def change
-    rename_column :wells_fargo_srp_sheets, :srp, :thefile
+  def up
+    unless column_exists? :wells_fargo_srp_sheets, :thefile
+      rename_column :wells_fargo_srp_sheets, :srp, :thefile
+    end
+  end
+
+  def down
+    unless column_exists? :wells_fargo_srp_sheets, :srp
+      rename_column :wells_fargo_srp_sheets, :thefile, :srp
+    end
   end
 end
